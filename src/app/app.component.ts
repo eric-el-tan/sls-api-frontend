f7013677f0beab3251a723999159b959fd24e4a8
etan221 <eric.tan@auckland.ac.nz>
2020-09-23 09:45:27 +1200

    getPosts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { Post } from './post';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sls-api-frontend';
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+
+  posts: Observable<Post[]>;
+
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts');
+  }
 }
