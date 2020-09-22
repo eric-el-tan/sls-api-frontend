2adfdff09b05cb702a3ac89874db95e04d6cf591
etan221 <eric.tan@auckland.ac.nz>
2020-09-23 10:15:37 +1200

    getPost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Post } from './post';
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -15,10 +15,16 @@ export class AppComponent {
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
 
   posts: Observable<Post[]>;
+  // post: Observable<any>;
 
   constructor(private http: HttpClient) { }
 
   getPosts() {
     this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts');
+  }
+  getPost() {
+
+    let params = new HttpParams().set('userId', '1');
+    this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts', { params });
   }
 }
